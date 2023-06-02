bcc0c3655d78002c97a2fb171ac3d87a97b88b8a
sparklyballs <sparklyballs@gmail.com>
2023-06-02 19:17:20 +0100

    revised and simplified linting script

--- linting-script.sh
+++ linting-script.sh
@@ -1,71 +1,32 @@
 #!/bin/bash
 
-# clear preexisting variables not set by job
-unset MOUNT_OPTIONS TEST_AREA SHELLCHECK_OPTIONS
-
-# clear preexising checkstyle files
-[[ -f "${WORKSPACE}"/shellcheck-result.xml ]] && rm "${WORKSPACE}"/shellcheck-result.xml
-
-# check for common locations and exit if not found
-if [[ ! -d "${WORKSPACE}"/root/etc/cont-init.d  && ! -d "${WORKSPACE}"/root/etc/services.d && \
-! -d "${WORKSPACE}"/init  && ! -d "${WORKSPACE}"/services ]]; then
-echo "no common files found, linting not required" exit 0
-fi
-
-if [[ ! -d "${WORKSPACE}"/root/etc/cont-init.d  && ! -d "${WORKSPACE}"/root/etc/services.d ]] && \
-[[ -d "${WORKSPACE}"/init && -d "${WORKSPACE}"/services ]]; then
-SHELLCHECK_OPTIONS="--format=checkstyle --shell=bash"
-MOUNT_OPTIONS="-v ${WORKSPACE}/init:/init -v ${WORKSPACE}/services:/services"
-TEST_AREA="init services"
-
-elif [[ ! -d "${WORKSPACE}"/root/etc/cont-init.d  && ! -d "${WORKSPACE}"/root/etc/services.d ]] && \
-[[ ! -d "${WORKSPACE}"/init && -d "${WORKSPACE}"/services ]]; then
-SHELLCHECK_OPTIONS="--format=checkstyle --shell=bash"
-MOUNT_OPTIONS="-v ${WORKSPACE}/services:/services"
-TEST_AREA="services"
-
-elif [[ ! -d "${WORKSPACE}"/root/etc/cont-init.d  && ! -d "${WORKSPACE}"/root/etc/services.d ]] && \
-[[ -d "${WORKSPACE}"/init && ! -d "${WORKSPACE}"/services ]]; then
-SHELLCHECK_OPTIONS="--format=checkstyle --shell=bash"
-MOUNT_OPTIONS="-v ${WORKSPACE}/init:/init"
-TEST_AREA="init"
-
-elif [[ -d "${WORKSPACE}"/root/etc/cont-init.d  && -d "${WORKSPACE}"/root/etc/services.d ]]; then
-SHELLCHECK_OPTIONS="--exclude=SC1008 --format=checkstyle --shell=bash"
-MOUNT_OPTIONS="-v ${WORKSPACE}/root:/root"
-TEST_AREA="root/etc/services.d root/etc/cont-init.d"
-
-elif [[ ! -d "${WORKSPACE}"/root/etc/cont-init.d  && -d "${WORKSPACE}"/root/etc/services.d ]]; then
-SHELLCHECK_OPTIONS="--exclude=SC1008 --format=checkstyle --shell=bash"
-MOUNT_OPTIONS="-v ${WORKSPACE}/root:/root"
-TEST_AREA="root/etc/services.d"
-
-elif [[ -d "${WORKSPACE}"/root/etc/cont-init.d  && ! -d "${WORKSPACE}"/root/etc/services.d ]]; then
-SHELLCHECK_OPTIONS="--exclude=SC1008 --format=checkstyle --shell=bash"
-MOUNT_OPTIONS="-v ${WORKSPACE}/root:/root"
-TEST_AREA="root/etc/cont-init.d"
-fi
+# pull newest images needed for linting
+docker pull sparklyballs/shellcheck:latest
+docker pull ghcr.io/hadolint/hadolint:latest
 
 # run shellcheck
-if [[ -d "${WORKSPACE}"/root/etc/cont-init.d || -d "${WORKSPACE}"/root/etc/services.d || \
--d "${WORKSPACE}"/init  || -d "${WORKSPACE}"/services ]];then
+SHELLCHECK_OPTIONS="--exclude=SC1008 --format=checkstyle --shell=bash"
+SHELLCHECK_EXCLUDES=( \
+"! -iname *.desktop" \
+"! -iname Dockerfile" \
+"! -iname jenkinsfile" \
+"! -iname *.json" \
+"! -iname *.md" \
+"! -iname pkgbuild*" \
+"! -iname *.txt" \
+"! -iname *.xml" \
+)
 
-docker pull koalaman/shellcheck-alpine
-
-docker run \
-	--rm=true -t \
-	${MOUNT_OPTIONS} \
-	koalaman/shellcheck-alpine \
-	find ${TEST_AREA} -type f -exec shellcheck ${SHELLCHECK_OPTIONS} {} + \
-	> ${WORKSPACE}/shellcheck-result.xml
-
-fi
+# shellcheck disable=SC2068,SC2086
+docker run --rm=true -t \
+-v ${WORKSPACE}:/test sparklyballs/shellcheck:latest \
+find /test -type f ${SHELLCHECK_EXCLUDES[@]} -not -path '*/\.*' -exec shellcheck ${SHELLCHECK_OPTIONS} {} + \
+> ${WORKSPACE}/shellcheck-result.xml
 
 [[ ! -f ${WORKSPACE}/shellcheck-result.xml ]] && echo "<?xml version='1.0' encoding='UTF-8'?><checkstyle version='4.3'></checkstyle>" > ${WORKSPACE}/shellcheck-result.xml
-sed -i 's/&#45;/-/g' ${WORKSPACE}/shellcheck-result.xml
+sed -i -e 's/&#45;/-/g' -e 's# >#>#g' -e 's# />#/>#g' -e 's#/test/##g' ${WORKSPACE}/shellcheck-result.xml
 
-docker pull ghcr.io/hadolint/hadolint
-
+# run hadolint
 docker run \
 	--rm=true -t \
 	-v ${WORKSPACE}/Dockerfile:/Dockerfile \
