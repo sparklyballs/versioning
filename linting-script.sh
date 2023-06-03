e2802223fdf547e730a78639b8ef2a22eae16ef8
sparklyballs <sparklyballs@gmail.com>
2023-06-03 01:13:42 +0100

    refine linting script, add extra shellcheck excludes

--- linting-script.sh
+++ linting-script.sh
@@ -5,25 +5,31 @@
 docker pull sparklyballs/shellcheck:latest
 docker pull ghcr.io/hadolint/hadolint:latest
 
-# run shellcheck
+# set variables/array
+HADOLINT_OPTIONS="--ignore DL3008 --ignore DL3013 --ignore DL3018 --ignore DL3028 --format json"
 SHELLCHECK_OPTIONS="--exclude=SC1008 --format=checkstyle --shell=bash"
 SHELLCHECK_EXCLUDES=( \
+"! -iname *.csv" \
+"! -iname *.dat" \
 "! -iname *.desktop" \
 "! -iname dockerfile" \
 "! -iname jenkinsfile" \
-"! -iname license" \
 "! -iname *.json" \
+"! -iname license" \
 "! -iname *.list" \
 "! -iname *.md" \
 "! -iname pkgbuild*" \
+"! -iname *.py" \
 "! -iname *.txt" \
 "! -iname *.xml" \
 )
 
-docker run --rm=true -t \
--v ${WORKSPACE}:/test sparklyballs/shellcheck:latest \
-find /test -type f ${SHELLCHECK_EXCLUDES[@]} -not -path '*/\.*' -exec shellcheck ${SHELLCHECK_OPTIONS} {} + \
-> ${WORKSPACE}/shellcheck-result.xml
+# run shellcheck
+docker run \
+	--rm=true -t \
+	-v ${WORKSPACE}:/test sparklyballs/shellcheck:latest \
+	find /test -type f ${SHELLCHECK_EXCLUDES[@]} -not -path '*/\.*' -exec shellcheck ${SHELLCHECK_OPTIONS} {} + \
+	> ${WORKSPACE}/shellcheck-result.xml
 
 [[ ! -f ${WORKSPACE}/shellcheck-result.xml ]] && echo "<?xml version='1.0' encoding='UTF-8'?><checkstyle version='4.3'></checkstyle>" > ${WORKSPACE}/shellcheck-result.xml
 sed -i -e 's/&#45;/-/g' -e 's# >#>#g' -e 's# />#/>#g' -e 's#/test/##g' ${WORKSPACE}/shellcheck-result.xml
@@ -32,9 +38,8 @@ sed -i -e 's/&#45;/-/g' -e 's# >#>#g' -e 's# />#/>#g' -e 's#/test/##g' ${WORKSPA
 docker run \
 	--rm=true -t \
 	-v ${WORKSPACE}/Dockerfile:/Dockerfile \
-	ghcr.io/hadolint/hadolint \
-	hadolint --ignore DL3008 --ignore DL3013 --ignore DL3018 --ignore DL3028 \
-	--format json /Dockerfile > ${WORKSPACE}/hadolint-result.xml
+	ghcr.io/hadolint/hadolint:latest \
+	hadolint ${HADOLINT_OPTIONS} /Dockerfile > ${WORKSPACE}/hadolint-result.xml
 
 # exit gracefully
 exit 0
